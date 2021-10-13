4248a485d2e9f38403ac04b64a44bfbeb437d952
Archit Khanna <architk.222@gmail.com>
2021-10-13 19:13:03 +0530

    Updates

---
+++ terraform/archit/lists-and-sets/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+
+  region = "eu-west-1"
+
+}
+
+resource "aws_iam_user" "tf_iam_users" {
+
+  count = length(var.names)
+  name  = var.names[count.index]
+
+}
